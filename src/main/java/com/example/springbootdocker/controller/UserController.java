b47d42070812a4e01628ee36f85858dbb763a019
SachPrecious <sachithrabandara49@gmail.com>
2022-10-13 09:16:59 +0530

    Dockerfile Added

--- src/main/java/com/example/springbootdocker/controller/UserController.java
+++ src/main/java/com/example/springbootdocker/controller/UserController.java
@@ -1,4 +1,15 @@
 package com.example.springbootdocker.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class UserController {
+
+@GetMapping("/message")
+public String getMessage(){
+    return "Welcome To Spring Docker";
+}
+
+
 }
